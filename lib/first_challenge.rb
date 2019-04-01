50b328272327b10a965601a1b9c550169fc86a5e
Nathan Richardson <nathan.richardson94@gmail.com>
2019-04-01 15:21:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,21 @@ def first_challenge
   }
 
   #your code here
+contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
 
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+    value.each do |flavor|
+    puts "#{flavor}"
+  end
+end
+  end
+end 
 
   #remember to return your newly altered contacts hash!
-  contacts
+  puts contacts
 end
 
+puts first_challenge
